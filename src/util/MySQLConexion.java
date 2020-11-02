e29a6ea2c44802a42a8629943910884c504da61d
xCR0V3R <57166198+xCR0V3R@users.noreply.github.com>
2020-11-02 00:52:47 -0500

    modificacion conexion bd

--- src/util/MySQLConexion.java
+++ src/util/MySQLConexion.java
@@ -4,9 +4,9 @@ public class MySQLConexion {
 	public static Connection getConexion(){ 
 		Connection con = null;
 		try { Class.forName("com.mysql.jdbc.Driver"); 
-		String url = "jdbc:mysql://localhost/bdtienda"; 
-		String usr = "root";
-		String psw = ""; 
+		String url = "jdbc:mysql://sql10.freemysqlhosting.net/sql10373934"; 
+		String usr = "sql10373934";
+		String psw = "RfWB6Hvmv3"; 
 		con = DriverManager.getConnection(url,usr,psw); 
 		System.out.println("conexion ok");
 		} catch (ClassNotFoundException ex)
