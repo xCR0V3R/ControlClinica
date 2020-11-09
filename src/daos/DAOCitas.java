f61328fba0c845784bfaa588a3e7645178b1d24e
Jose Alejandro <Jose Alejandro@LAPTOP-4929H4I2>
2020-11-08 21:04:35 -0500

    Clase Citas DAO Citas TERMINADO

--- src/daos/DAOCitas.java
+++ src/daos/DAOCitas.java
@@ -10,30 +10,27 @@ import modelo.*;
 import vistaa.*;
 
 public class DAOCitas {
-    /*
+    
     //LISTAR UNA CITA
     
     public List<Cita> lisCita() {
-        List<Medico> lis = new ArrayList<>();
+        List<Cita> lis = new ArrayList<>();
         Connection conn = null;
 
         try {
             conn = MySQLConexion.getConexion();
-            String sql = "select * from Medico";
+            String sql = "select id, feccit, hora, nomp, estado from Citas";
             PreparedStatement st = conn.prepareStatement(sql);
             ResultSet rs = st.executeQuery();
             
             while (rs.next()) {
-                Medico a = new Medico();
-                a.setCodmed(rs.getString(1));
-                a.setCodes(rs.getString(2));
-                a.setIptip(rs.getString(3));
-                a.setFecha(rs.getString(4));
-                a.setCorreo(rs.getString(5));
-                a.setPswd(rs.getString(6));
-                a.setNombre(rs.getString(7));
-                a.setDni(rs.getInt(8));
-                lis.add(a);
+                Cita c = new Cita();
+                c.setIdCita(rs.getString(1));
+                c.setDiacit(rs.getString(2));
+                c.setHoracit(rs.getString(3));
+                c.setNompac(rs.getString(4));
+                c.setEstadopac(rs.getString(5));
+                lis.add(c);
             }
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -53,32 +50,32 @@ public class DAOCitas {
     //GENERAR CÓDIGO DE ALUMNO SIN STORE PROCEDURE
     public String generaCod(){
         String cod="A0001";
-        if(listotAlu().size()==0){
+        if(lisCita().size()==0){
             return cod;
         }else{
-            int fin=listotAlu().size()-1;//Definir en números cual es el último número del código
-            cod=listotAlu().get(fin).getCoda();//Una vez definido el número, lo ubicas en el sistema de "A000?"
+            int fin=lisCita().size()-1;//Definir en números cual es el último número del código
+            cod=lisCita().get(fin).getIdCita();//Una vez definido el número, lo ubicas en el sistema de "A000?"
             int nro=Integer.parseInt(cod.substring(1))+1;//Aquí le aumentas el valor de 1 a la parte numérica
             DecimalFormat sd=new DecimalFormat("0000");//Aquí se define el formato del número "A000?"
             cod="A"+sd.format(nro);//Se usa el formato "A000?" aumentándole el número que obtuvimos consecuente al último valor
+            return cod;
         }
-        return c
+    }
     
-    public void addCita(Usuario u) {
+    public void addCita(Cita c) {
         int resp = 0;           
         Connection conn = null; 
         try {
-            String sql = "Insert into Cita values(?,?,?,?,?,?,?,?)";
+            String sql = "Insert into Cita values(?,?,?,?,?,?,?)";
             conn = MySQLConexion.getConexion();
             PreparedStatement st = conn.prepareStatement(sql);
-            st.setString(1,genCodMed());
-            st.setString(2,a.getCodes());
-            st.setString(3, a.getIptip());
-            st.setString(4, a.getFecha());
-            st.setString(5, a.getCorreo());
-            st.setString(6, a.getPswd());
-            st.setString(7, a.getNombre());
-            st.setInt(8, a.getDni());
+            st.setString(1,c.getCodmed());
+            st.setString(2, c.getDnipac());
+            st.setString(3, c.getEstadopac());
+            st.setString(4, c.getDiacit());
+            st.setString(5, c.getHoracit());
+            st.setString(6, generaCod());
+            st.setString(7, c.getNompac());
             resp = st.executeUpdate();
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -91,5 +88,5 @@ public class DAOCitas {
             }
         }
 
-    }*/
+    }
 }
--- src/modelo/Cita.java
+++ src/modelo/Cita.java
@@ -1,14 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
-/**
- *
- * @author Jose Alejandro
- */
 public class Cita {
+    private String idCita;
+    private String nompac;
+    private String diacit;
+    private String horacit;
+    private String estadopac;
+    private String dnipac;
+    private String codmed;
+
+    public Cita() {
+    }
+    
+    public String getIdCita() {
+        return idCita;
+    }
+
+    public void setIdCita(String idCita) {
+        this.idCita = idCita;
+    }
+
+    public String getNompac() {
+        return nompac;
+    }
+
+    public void setNompac(String nompac) {
+        this.nompac = nompac;
+    }
+
+    public String getDiacit() {
+        return diacit;
+    }
+
+    public void setDiacit(String diacit) {
+        this.diacit = diacit;
+    }
+
+    public String getHoracit() {
+        return horacit;
+    }
+
+    public void setHoracit(String horacit) {
+        this.horacit = horacit;
+    }
+
+    public String getEstadopac() {
+        return estadopac;
+    }
+
+    public void setEstadopac(String estadopac) {
+        this.estadopac = estadopac;
+    }
+
+    public String getDnipac() {
+        return dnipac;
+    }
+
+    public void setDnipac(String dnipac) {
+        this.dnipac = dnipac;
+    }
+
+    public String getCodmed() {
+        return codmed;
+    }
+
+    public void setCodmed(String codmed) {
+        this.codmed = codmed;
+    }
+    
     
 }
