d3ce1ab84b85c2d327fddc9395186949c9e40e4b
Jose Alejandro <Jose Alejandro@LAPTOP-4929H4I2>
2020-11-10 11:38:55 -0500

    Botones crear cita Aun no terminado

--- src/metodos/mAdmi.java
+++ src/metodos/mAdmi.java
@@ -1,13 +1,27 @@
 
 
 package metodos;
+import vistaa.*;import controlador.*;
+import java.awt.Desktop;
+import java.net.URI;
+import javax.swing.*;
+import modelo.*;import daos.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
-import javax.swing.JFrame;
-import vistaa.pAdmi; import metodos.mLog;
 
 
 public class mAdmi {
+    /*
+    String cNom; String cFec; private String dTip;int cDNI;private String dCorreo;private String dPswd;
+    private String dEsp;
+    
+    SimpleDateFormat fordia=new SimpleDateFormat("yyyy-MM-dd");
+    SimpleDateFormat forhora=new SimpleDateFormat("yyyy-MM-dd");*/
+    
     mLog log=new mLog();
+    
     
     public void inicializarAdmi(pAdmi ad){ 
          ad.setTitle("ADMINISTRADOR");
@@ -35,7 +49,21 @@ public class mAdmi {
          ad.setLocationRelativeTo(null);
         log.esIcono(ad);
     }
-    
- 
+    /*
+    public void obtenerData(){
+        cNom=log.txtNombr.getText();
+        cFec=formato.format(this.log.dateNacimiento.getDate());
+        cDNI=Integer.parseInt(this.log.txtDNI.getText());
+        dCorreo=log.txCorreo.getText();
+        dPswd=log.txContra.getText();
+        String tipo="";
+        tipo=log.jcbTipoUser.getSelectedItem().toString();
+        if(tipo.equals("Medico")) dTip="U002";
+        if(tipo.equals("Administrador")) dTip="U001";
+        if(tipo.equals("-Seleccionar-")) dTip="U000";
+        if(dTip.equals("U002")){
+            dEsp=log.jcbxEspecialidad1.getSelectedItem().toString();
+        }
+        }*/
     
 }
